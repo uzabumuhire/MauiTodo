841b19c5b513c13533605f839c078038f8bf5985
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2022-05-24 01:25:12 +0200

    Connect the UI to the database

--- MauiTodo/MainPage.xaml.cs
+++ MauiTodo/MainPage.xaml.cs
@@ -1,10 +1,48 @@
-﻿namespace MauiTodo;
+﻿using MauiTodo.Data;
+using MauiTodo.Models;
+
+namespace MauiTodo;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+	// To-do items to be displayed on screen
+    string _todoListData = string.Empty;
+
+    readonly Database _database;
+    public MainPage()
 	{
 		InitializeComponent();
-	}
+		_database = new Database();
+        _ = Initialise();
+    }
+
+    async Task Initialise()
+    {
+        var todos = await _database.GetTodos();
+
+        foreach (var todo in todos)
+            _todoListData += $"{todo.Title} - {todo.Due:f}{Environment.NewLine}";
+
+        TodosLabel.Text = _todoListData;
+    }
+
+    async void Button_Clicked(object sender, EventArgs e)
+    {
+        var todo = new TodoItem
+        {
+            Due = DueDatePicker.Date,
+            Title = TodoTitleEntry.Text
+        };
+
+        var inserted = await _database.AddTodo(todo);
+
+        if (inserted != 0)
+        {
+            _todoListData += $"{todo.Title} - {todo.Due:f}{Environment.NewLine}";
+            TodosLabel.Text = _todoListData;
+            TodoTitleEntry.Text = string.Empty;
+            DueDatePicker.Date = DateTime.Now;
+        }
+    }
 }
 
