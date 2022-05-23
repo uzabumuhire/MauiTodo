509718337b20d51be64d7c5651fe212bb71d7ce5
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2022-05-24 00:39:10 +0200

    Add secure storage management functionality

---
+++ MauiTodo/Data/Database.cs
@@ -0,0 +1,68 @@
+﻿using SQLite;
+
+using MauiTodo.Models;
+
+namespace MauiTodo.Data
+{
+    public class Database
+    {
+        private readonly SQLiteAsyncConnection _connection;
+
+        public Database()
+        {
+            var dataDir = FileSystem.AppDataDirectory;
+            var databasePath = Path.Combine(dataDir, "MauiTodo.db");
+
+            string _dbEncryptionKey = SecureStorage.GetAsync("dbKey").Result;
+
+            if (string.IsNullOrEmpty(_dbEncryptionKey))
+            {
+                Guid g = new();
+                _dbEncryptionKey = g.ToString();
+                SecureStorage.SetAsync("dbKey", _dbEncryptionKey);
+            }
+
+            var dbOptions = new SQLiteConnectionString(databasePath, true, key: _dbEncryptionKey);
+
+            _connection = new SQLiteAsyncConnection(dbOptions);
+
+            // Using a discard ( _ ) will then run the method
+            // in a thread without blocking the UI since we
+            // can't have async constructors.
+            _ = Initialise();
+        }
+
+        private async Task Initialise()
+        {
+            await _connection.CreateTableAsync<TodoItem>();
+        }
+
+        public async Task<List<TodoItem>> GetTodos()
+        {
+            return await _connection.Table<TodoItem>().ToListAsync();
+
+        }
+
+        public async Task<TodoItem> GetTodo(int id)
+        {
+            var query = _connection.Table<TodoItem>().Where(t => t.Id == id);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<int> AddTodo(TodoItem item)
+        {
+            return await _connection.InsertAsync(item);
+        }
+
+        public async Task<int> DeleteTodo(TodoItem item)
+        {
+            return await _connection.DeleteAsync(item);
+        }
+
+        public async Task<int> UpdateTodo(TodoItem item)
+        {
+            return await _connection.UpdateAsync(item);
+        }
+    }
+}
