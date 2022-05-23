719b4203897586bc80fe7ef800aeef5eff0e0138
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2022-05-23 23:47:10 +0200

    Define the data model

---
+++ MauiTodo/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiTodo.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Due { get; set; }
+        public bool Done { get; set; }
+    }
+}
