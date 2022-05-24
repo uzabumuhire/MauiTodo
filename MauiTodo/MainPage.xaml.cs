73189df3ac7089c67f30195c045190601c2aea1d
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2022-05-24 14:34:34 +0200

    Bind the CollectionView to an ObservableCollection of TodoItems

--- MauiTodo/MainPage.xaml.cs
+++ MauiTodo/MainPage.xaml.cs
@@ -1,29 +1,37 @@
-﻿using MauiTodo.Data;
+﻿using System.Collections.ObjectModel;
+
+using MauiTodo.Data;
 using MauiTodo.Models;
 
 namespace MauiTodo;
 
 public partial class MainPage : ContentPage
 {
-	// To-do items to be displayed on screen
-    string _todoListData = string.Empty;
+    public ObservableCollection<TodoItem> Todos { get; set; } = new();
 
     readonly Database _database;
+
     public MainPage()
 	{
 		InitializeComponent();
-		_database = new Database();
+        // The ObservableCollection is set as the binding source
+        // for a CollectionView.
+        TodosCollection.ItemsSource = Todos;
+
+        _database = new Database();
         _ = Initialise();
+        
     }
 
     async Task Initialise()
     {
         var todos = await _database.GetTodos();
 
+        // Add to-do items to the ObservableCollection
+        // when the page initializes. This will automatically
+        // update the CollectionView in the UI.
         foreach (var todo in todos)
-            _todoListData += $"{todo.Title} - {todo.Due:f}{Environment.NewLine}";
-
-        TodosLabel.Text = _todoListData;
+            Todos.Add(todo);
     }
 
     async void Button_Clicked(object sender, EventArgs e)
@@ -38,8 +46,12 @@ public partial class MainPage : ContentPage
 
         if (inserted != 0)
         {
-            _todoListData += $"{todo.Title} - {todo.Due:f}{Environment.NewLine}";
-            TodosLabel.Text = _todoListData;
+            // Add to-do item to the ObservableCollection
+            // when a user adds a new to-do item. This will
+            // automatically update the CollectionView in the UI.
+            Todos.Add(todo);
+
+            // Reset the form
             TodoTitleEntry.Text = string.Empty;
             DueDatePicker.Date = DateTime.Now;
         }
